ce793dd492893f9ae1c878c8baa52a8d9e1c0226
ajg7 <ajgebara@gmail.com>
2020-07-17 09:41:21 -0600

    finished task 2

--- index.js
+++ index.js
@@ -216,6 +216,12 @@ console.log(artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+const correction = (objs, index, prop, value) => {
+    objs[index].prop = value;
+    return value;
+}
+
+console.log(correction(artists, 9, "name", "Vincent Van Gogh"));
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
